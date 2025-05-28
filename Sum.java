defc02a5526462720139b5698a7094aa263d3d9d
Arnav Bhattacharya <atzgg132@gmail.com>
2025-05-28 15:13:42 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,17 @@
+/**
+ * A simple Java program to add two hardcoded numbers and display the result.
+ */
+public class Sum {
+    public static void main(String[] args) {
+        // Hardcoded numbers
+        int num1 = 25;
+        int num2 = 30;
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+        
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
